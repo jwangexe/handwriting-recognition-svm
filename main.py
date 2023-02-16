ee1b563132081457fe2118aa88c971c75ad1ce1d
Jun Wang <wjun9189@gmail.com>
2023-02-16 17:12:19 +0000

    Split training and testing data in python

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("traindata.csv")
 
 x, y = data.iloc[:, 1:], data.iloc[:, 0]
-print(x, y)
+print(x.head(), y.head())
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=30)
